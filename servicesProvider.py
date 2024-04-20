c7a243f8a557257cab8fb80c31e4941b31e59bb8
Luli <114842776+lutriana@users.noreply.github.com>
2024-04-20 12:26:02 -0500

    Update servicesProvider.py
    Documentacion de codigo y API añadida

--- servicesProvider.py
+++ servicesProvider.py
@@ -1,14 +1,25 @@
 from patientRegistration import ClinicHistory
-"""
-el provedor de servicios tiene acceso a los resultados de lab e imagenes
-NOTA: relacionar atributos especificos a los provedores de servicios
 
-"""
 class ServicesProvider:
-    def __init__(self, id, name, specialty, phone, address):
+    """
+    Representa un proveedor de servicios que tiene acceso a los resultados de laboratorio
+    e imágenes de los pacientes. Esta clase mantiene detalles específicos del proveedor.
+    """
+    
+    def _init_(self, id, name, specialty, phone, address):
+        """
+        Inicializa un nuevo proveedor de servicios con los detalles especificados.
+        
+        Args:
+            id (int): Identificador único para el proveedor de servicios.
+            name (str): Nombre del proveedor de servicios.
+            specialty (str): Especialidad médica del proveedor.
+            phone (str): Número de teléfono de contacto del proveedor.
+            address (str): Dirección de la oficina del proveedor.
+        """
         self.id_provider = id
         self.name_provider = name
-        self.specialty= specialty
+        self.specialty = specialty
         self.phone_provider = phone
         self.address_provider = address
         
