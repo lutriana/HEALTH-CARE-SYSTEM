7e2f988197f1b48d6983030248438adc032039d8
Luli <114842776+lutriana@users.noreply.github.com>
2024-04-20 12:23:19 -0500

    Update billing.py
    Documentacion de codigo y API añadidas

--- billing.py
+++ billing.py
@@ -1,10 +1,26 @@
 from patientRegistration import Pacient
 
 class Billing:
-    def __init__(self ):
+    """
+    Representa un sistema de facturación que gestiona los pagos y los asocia con
+    pacientes específicos.
+    """
+    
+    def _init_(self):
+        """
+        Inicializa el sistema de facturación con una lista vacía para pagos y
+        un marcador de posición para los datos de los pacientes.
+        """
         self.payment = []
         self.data = Pacient()
     
     def add_payment(self, payment_method, dataPayment):
-        self.payment = payment_method
+        """
+        Registra un nuevo pago en el sistema.
+        
+        Args:
+            payment_method (str): Método a través del cual se realizó el pago.
+            dataPayment (Pacient): Datos del paciente asociados con el pago.
+        """
+        self.payment.append(payment_method)
         self.data = dataPayment
